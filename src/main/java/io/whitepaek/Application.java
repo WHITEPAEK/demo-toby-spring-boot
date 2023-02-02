e7c04520044d2e07e24717ba9a8361c8ea30d0bc
SEUNGJOO PAEK <atomsjp@gmail.com>
2023-02-02 22:48:49 +0900

    refactor: Bean Factory Method 적용

--- src/main/java/io/whitepaek/Application.java
+++ src/main/java/io/whitepaek/Application.java
@@ -3,14 +3,28 @@ package io.whitepaek;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class Application {
+
+    // Factory Method
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         // Spring Container
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             // Hook
             @Override
             protected void onRefresh() {
@@ -26,8 +40,7 @@ public class Application {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(Application.class);
         applicationContext.refresh(); // Template Method Pattern
     }
 
