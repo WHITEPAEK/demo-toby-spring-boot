c7c506aaea4ebf971481cab8450a961e619bf371
SEUNGJOO PAEK <atomsjp@gmail.com>
2023-02-02 22:41:19 +0900

    refactor: 스프링 컨테이너로 통합

--- src/main/java/io/whitepaek/Application.java
+++ src/main/java/io/whitepaek/Application.java
@@ -10,19 +10,25 @@ public class Application {
 
     public static void main(String[] args) {
         // Spring Container
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            // Hook
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                // Servlet Container
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
-
-        // Servlet Container
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        webServer.start();
+        applicationContext.refresh(); // Template Method Pattern
     }
 
 }
