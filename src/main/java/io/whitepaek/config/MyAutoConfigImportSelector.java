de8f045eff2f0c2fa002fa44b7b28030e3941bd0
SEUNGJOO PAEK <atomsjp@gmail.com>
2023-02-05 18:13:13 +0900

    refactor: 자동 구성 정보 파일 분리

--- src/main/java/io/whitepaek/config/MyAutoConfigImportSelector.java
+++ src/main/java/io/whitepaek/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,38 @@
 package io.whitepaek.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "io.whitepaek.config.autoconfig.DispatcherServletConfig",
-                "io.whitepaek.config.autoconfig.TomcatWebServerConfig"
-        };
+//        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+
+        List<String> autoConfigs = new ArrayList<>();
+
+//        for (String candidates : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+//            autoConfigs.add(candidates);
+//        }
+//        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(candidate ->
+//                autoConfigs.add(candidate)
+//        );
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
+//        return autoConfigs.stream().toArray(String[]::new);
+//        return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
     }
 
 }
