2d9e60bb4fed6d9af4d351021d0d205d99de4d8f
SEUNGJOO PAEK <atomsjp@gmail.com>
2023-02-05 19:50:15 +0900

    refactor: Class 포함 여부에 따른 @Conditional 동작

--- src/main/java/io/whitepaek/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/io/whitepaek/config/autoconfig/JettyWebServerConfig.java
@@ -1,16 +1,13 @@
 package io.whitepaek.config.autoconfig;
 
+import io.whitepaek.config.ConditionalMyOnClass;
 import io.whitepaek.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
@@ -18,10 +15,4 @@ public class JettyWebServerConfig {
         return new JettyServletWebServerFactory();
     }
 
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
-    }
 }
