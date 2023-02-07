0fddd8599937d333101ccbf20b33df6ea1c0bd5a
SEUNGJOO PAEK <atomsjp@gmail.com>
2023-02-07 11:12:31 +0900

    feat: JdbcTemplate, 트랜잭션 매니저 구성

---
+++ src/test/java/io/whitepaek/hello/JdbcTemplateTest.java
@@ -0,0 +1,30 @@
+package io.whitepaek.hello;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists member(name varchar(50) primary key, count int)");
+    }
+
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into member values(?, ?)", "Toby", 3);
+        jdbcTemplate.update("insert into member values(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from member", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+
+}
